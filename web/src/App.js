7236388c0b79321bc50bcb88e9f3019da650cf88
vortlx <jkdsgjksdjg@gmail.com>
2020-09-26 18:28:32 +0300

    Blya, update

--- web/src/App.js
+++ web/src/App.js
@@ -4,9 +4,11 @@ import {Jutsu} from "react-jutsu"
 class App extends React.Component {
     render() {
         return <Jutsu
-                    domain = {"meet.jit.si"}
-                    roomName = {"HAHAHA"}
-                />
+            domain={"meet.jit.si"}
+            roomName={"hsdighsi745983nuiuer8ou"}
+            displayName={"FuckingClient"}
+            containerStyles={{ width: '100%', height: "1000px"}}
+        />
     }
 }
 
