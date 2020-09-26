3865552407871786e9d3d54a97714f2974f320f2
vortlx <jkdsgjksdjg@gmail.com>
2020-09-26 17:02:51 +0300

    Added default Jitsi connection to room by JitsiApi

---
+++ web/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Jutsu} from "react-jutsu"
+
+function App() {
+    return <Jutsu
+    domain={"meet.jit.si"}
+    roomName={"HAHAHA"}/>
+}
+
+export default App;
