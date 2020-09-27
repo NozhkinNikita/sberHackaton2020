f1f2087868a36a927abd9dbd633777fbb4f7344b
NikitaNozhkin <you_mazafaka@mail.ru>
2020-09-27 19:44:14 +0300

    web

---
+++ web/src/app.jsx
@@ -0,0 +1,67 @@
+import React, {Component} from "react";
+import {BrowserRouter, Redirect, Route, Switch,Router} from "react-router-dom";
+import AdminLayout from "./layouts/Admin";
+import Login from "./layouts/Login";
+
+export class App extends Component {
+    constructor() {
+        super();
+
+        this.setAuth = this.setAuth.bind(this);
+
+
+        this.state = {
+            auth: this.hasUser()
+        };
+    }
+    hasUser() {
+        return localStorage.getItem('user') != null;
+    }
+
+    setAuth(value){
+        console.log(222)
+        console.log(this)
+        console.log(this.state)
+        this.setState({auth:value})
+
+    }
+
+    render() {
+        return ((this.state.auth) ?
+
+
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/admin" render={
+
+
+                            props => <AdminLayout {...props} action={this.setAuth} />
+
+                        }/>
+                        <Redirect from="/" to="/admin/dashboard"/>
+                    </Switch>
+                </BrowserRouter>
+
+                    :
+
+                    <BrowserRouter>
+                        <Switch>
+                            <Route path="/login" render={
+
+
+                                props => <Login {...props}  action={this.setAuth}/>
+
+                            }/>
+                            <Redirect from="/" to="/login"/>
+                        </Switch>
+                    </BrowserRouter>
+
+
+
+
+
+        );
+    }
+}
+
+export default App;
