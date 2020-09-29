780512de36f860675765b1a5e4146c5f85cc6990
belov_000 <belov.da@live.com>
2020-09-29 13:14:04 +0300

    cors

--- server/controller/src/main/java/com/hackathon2020/security/SecurityConfig.java
+++ server/controller/src/main/java/com/hackathon2020/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -97,7 +98,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/login");
+        web.ignoring().antMatchers("/login")
+                .antMatchers(HttpMethod.OPTIONS, "/**");
     }
 
     @Bean
@@ -150,7 +152,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         authProvider.setPasswordEncoder(encoder());
         return authProvider;
     }
-
 
 
 }
