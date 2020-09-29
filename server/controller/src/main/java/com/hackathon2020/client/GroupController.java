7190d5f84f248498bd4999dbf22a2a28e2630c4f
belov_000 <belov.da@live.com>
2020-09-29 17:17:54 +0300

    controllers blya

--- server/controller/src/main/java/com/hackathon2020/client/GroupController.java
+++ server/controller/src/main/java/com/hackathon2020/client/GroupController.java
@@ -1,15 +1,14 @@
 package com.hackathon2020.client;
 
 import com.hackathon2020.dao.GroupDao;
-import com.hackathon2020.domain.Group;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @CrossOrigin
 @Controller
@@ -20,12 +19,12 @@ public class GroupController {
     private GroupDao groupDao;
 
     @GetMapping
-    public List<Group> getGroups() {
-        return groupDao.getAll();
+    public ResponseEntity<?> getGroups() {
+        return new ResponseEntity<>(groupDao.getAll(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
-    public Group getGroupById(@PathVariable String id) {
-        return groupDao.getById(id);
+    public ResponseEntity<?> getGroupById(@PathVariable String id) {
+        return new ResponseEntity<>(groupDao.getById(id), HttpStatus.OK);
     }
 }
--- server/controller/src/main/java/com/hackathon2020/client/ServiceController.java
+++ server/controller/src/main/java/com/hackathon2020/client/ServiceController.java
@@ -1,8 +1,8 @@
 package com.hackathon2020.client;
 
 import com.hackathon2020.dao.ServiceDao;
-import com.hackathon2020.domain.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @CrossOrigin
 @Controller
@@ -21,19 +19,23 @@ public class ServiceController {
     @Autowired
     private ServiceDao serviceDao;
 
-    @GetMapping(value = "/")
-    public List<Service> getServices() {
-        return serviceDao.getAll();
+    @GetMapping()
+    public ResponseEntity<?> getServices() {
+        return new ResponseEntity<>(serviceDao.getAll(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
-    public Service getById(@PathVariable String id) {
-        return serviceDao.getById(id);
+    public ResponseEntity<?> getById(@PathVariable String id) {
+        try {
+            return new ResponseEntity<>(serviceDao.getById(id), HttpStatus.OK);
+        } catch (Throwable e) {
+            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping(value = "/groups/{groupId}")
-    public List<Service> getByGroupId(@PathVariable String groupId) {
-        return serviceDao.getByGroupId(groupId);
+    public ResponseEntity<?> getByGroupId(@PathVariable String groupId) {
+        return new ResponseEntity<>(serviceDao.getByGroupId(groupId), HttpStatus.OK);
     }
 
     @PostMapping(value = "/{id}/call")
